9f049cfb74cd009e90b43ab550cb44bdcabd363c
Chealy-13 <chris.healy33@gmail.com>
2024-10-08 14:25:59 +0100

    undid test

--- src/main/java/com/example/musiclibrary/sql/musicLibrary.sql
+++ src/main/java/com/example/musiclibrary/sql/musicLibrary.sql
@@ -7,7 +7,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
     userId   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    test int null,
     username VARCHAR(50)         NOT NULL,
     password VARCHAR(100)        NOT NULL,
     email    VARCHAR(100) UNIQUE NOT NULL
