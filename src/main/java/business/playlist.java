94ee51a436f89c3d8db7ac2717af92259f88bf9a
JulieOlamijuwon <d00215779@student.dkit.ie>
2024-10-11 03:34:35 +0100

    Commit new playlist class

---
+++ src/main/java/business/playlist.java
@@ -0,0 +1,16 @@
+package business;
+
+public class playlist {
+    /*
+    CREATE TABLE playlist
+(
+    playlistId INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userId     INT UNSIGNED NOT NULL,
+    name       VARCHAR(50),
+    type       INT(2),
+
+    CONSTRAINT fk_playlist_user FOREIGN KEY (userId) REFERENCES users(userId)
+);
+    */
+
+}
