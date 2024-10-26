6536e2c581e8db235b2a405f6a014e96259ace7d
Chealy-13 <chris.healy33@gmail.com>
2024-10-26 18:55:55 +0100

    Created Artist class with lombok used for getters setters and constructors

---
+++ src/main/java/business/Artist.java
@@ -0,0 +1,14 @@
+package business;
+import lombok.*;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Artist {
+//    CREATE TABLE artists
+//            (
+//                    artistId INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//                    name     VARCHAR(30) NOT NULL
+//);
+    private int artistID;
+    private String artistName;
+}
