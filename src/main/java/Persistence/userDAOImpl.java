744eeee4877d5fe36f8a35f4e228d598715a7853
Damian0222 <d00229247@student.dkit.ie>
2024-10-27 11:52:44 +0000

    Useranames mathod takes the name as input and then searches if exists. Created a preparedstatement safely inserts username in place. - userDAOImpl

--- src/main/java/Persistence/userDAOImpl.java
+++ src/main/java/Persistence/userDAOImpl.java
@@ -1,8 +1,12 @@
 package Persistence;
 
 import business.user;
+import org.apache.catalina.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public abstract class userDAOImpl implements userDAO {
     private final Connection connection;
@@ -14,9 +18,13 @@ public abstract class userDAOImpl implements userDAO {
     @Override
     public user usernames(String username) {
         String sql = "SELECT * FROM users WHERE username = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
 
-    }
-}
+
+        }
+
 
 
 
