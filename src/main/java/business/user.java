b3d123c9d6b3fb9d66d85e8e5b271ff7b80ff11a
Damian0222 <d00229247@student.dkit.ie>
2024-10-29 08:53:51 +0000

    Updated usernames and save methods - userDAOImpl

--- src/main/java/business/user.java
+++ src/main/java/business/user.java
@@ -3,6 +3,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a user in the system with basic details such as user ID, username, password, and email.
+ * This class is a Data Transfer Object (DTO) used for handling user data between different parts of the application.
+ */
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
