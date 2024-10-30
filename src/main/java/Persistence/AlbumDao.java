5e3c7329149a0b1a48483bf64c4dcc77394d9b8c
Chealy-13 <chris.healy33@gmail.com>
2024-10-30 12:09:30 +0000

    removed abstract from getByAlbumId

--- src/main/java/Persistence/AlbumDao.java
+++ src/main/java/Persistence/AlbumDao.java
@@ -5,7 +5,7 @@ import business.Album;
 import java.util.List;
 
 public interface AlbumDao {
-    public abstract Album getByAlbumId(int albumId);
+    public Album getByAlbumId(int albumId);
 
     List<Album> getAlbumsByArtistId(int artistId);
 }
