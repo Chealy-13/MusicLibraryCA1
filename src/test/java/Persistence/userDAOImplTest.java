37530111d6d0f3df79ab6b5629cd238d90fd1a47
Damian0222 <d00229247@student.dkit.ie>
2024-10-30 16:01:07 +0000

    Completed test for the ValidateCCinfo method, shows valid and invalid inputs - userDAOImplTest

--- src/test/java/Persistence/userDAOImplTest.java
+++ src/test/java/Persistence/userDAOImplTest.java
@@ -6,8 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class userDAOImplTest {
 
+    private final userDAOImpl userDao = new userDAOImpl(null);
     @Test
-    void validateCCInfo() {
+    void testValidateCCInfo() {
+        // Valid user input test.
+        assertTrue(userDao.validateCCInfo("1234567812345678", "12/25", "123"));
+
+        // Invalid card number(must be 16 digits)
+        assertFalse(userDao.validateCCInfo("12345678", "12/25", "123"));
+
+        // Invalid expiry date(must be a valid month from 1-12
+        assertFalse(userDao.validateCCInfo("1234567812345678", "13/25", "123"));
+
+        // Invalid CVV(must be 3 or 4 digits,
+        assertFalse(userDao.validateCCInfo("1234567812345678", "12/25", "12"));
+
+        // Valid CVV with 4 digits
+        assertTrue(userDao.validateCCInfo("1234567812345678", "12/25", "1234"));
     }
 
     @Test
