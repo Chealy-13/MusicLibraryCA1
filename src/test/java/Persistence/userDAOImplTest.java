6652ac6432d27e3497fccee449e41d85a0614d0b
Damian0222 <d00229247@student.dkit.ie>
2024-10-30 15:41:41 +0000

    Created a test class for my impl methods.- MusicLibraryApplication

---
+++ src/test/java/Persistence/userDAOImplTest.java
@@ -0,0 +1,24 @@
+package Persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class userDAOImplTest {
+
+    @Test
+    void validateCCInfo() {
+    }
+
+    @Test
+    void registerU() {
+    }
+
+    @Test
+    void loginU() {
+    }
+
+    @Test
+    void deleteByUsername() {
+    }
+}
