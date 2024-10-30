27dd2feb698c0be70f9a6020640f811eae73ac8d
Chealy-13 <chris.healy33@gmail.com>
2024-10-30 14:49:46 +0000

    Created getAllArtists

--- src/main/java/Persistence/ArtistDao.java
+++ src/main/java/Persistence/ArtistDao.java
@@ -2,6 +2,10 @@ package Persistence;
 
 import business.Artist;
 
+import java.util.List;
+
 public interface ArtistDao {
     Artist getArtistByArtistId(int artistId);
+
+    List<Artist> getAllArtists();
 }
