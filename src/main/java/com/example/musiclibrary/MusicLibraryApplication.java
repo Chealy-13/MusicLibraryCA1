86adf718ce63dfc3e300e7743f7068503d64dd3a
Damian0222 <d00229247@student.dkit.ie>
2024-10-30 11:14:00 +0000

    Updated the app so its more user friendly, added creditcard validation to the RegisterU method- userDAOImpl

--- src/main/java/com/example/musiclibrary/MusicLibraryApplication.java
+++ src/main/java/com/example/musiclibrary/MusicLibraryApplication.java
@@ -11,21 +11,47 @@ public class MusicLibraryApplication {
 	private static final String url = "jdbc:mysql://localhost:3306/musiclibary";
 	private static final String username = "root";
 	private static final String password = "";
+	private static userDAOImpl userDAO;
+	private static final Scanner scanner = new Scanner(System.in);
 
-	private userDAO userDAO;
 
-	public MusicLibraryApplication() {
+	public static void main(String[] args) {
+
 		try {
-			Connection connection = DriverManager.getConnection(url, username, password);
-			userDAO = new userDAOImpl(connection) {
-			};
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/musiclibary", "root", "");
+			userDAO = new userDAOImpl(connection);
+			welcomeApp();
 		} catch (SQLException e) {
+			System.out.println("Failed to connect to the database.");
 			e.printStackTrace();
 		}
 	}
 
+	private static void welcomeApp() {
+		MusicLibraryApplication a = new MusicLibraryApplication();
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("1. Register: ");
+			System.out.println("2. Login: ");
+			System.out.println("3. ");
+			System.out.println("4. ");
+			System.out.println("5. ");
 
-	public void register() {
+			int choice1 = scanner.nextInt();
+			scanner.nextLine();
+
+			switch (choice1) {
+				case 1:
+					a.RegisterU1();
+					break;
+				case 2:
+					a.LoginU();
+					break;
+			}
+		}
+	}
+
+	public void RegisterU1() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Register user: ");
 
@@ -38,53 +64,41 @@ public class MusicLibraryApplication {
 		System.out.println("Please provide an email address: ");
 		String email = scanner.nextLine();
 
-		user newU = new user(0, username, password, email);
-		if (userDAO.save(newU)) {
-			System.out.println("Registration successful!");
+		System.out.print("Enter your credit card number: ");
+		String cardNum = scanner.nextLine();
+
+		System.out.print("Enter your expiry date (MM/YY): ");
+		String expiryDate = scanner.nextLine();
+
+		System.out.print("Enter your CVV: ");
+		String cvv = scanner.nextLine();
+		if (userDAO.validateCCInfo(cardNum, expiryDate, cvv)) {
+
+			if (userDAO.RegisterU(username, password, email)) {
+				System.out.println("Registration successful!");
+			} else {
+				System.out.println("Unsuccessful!, please try again!");
+			}
 		} else {
-			System.out.println("Unsuccessful registration, please try again!");
+			System.out.println("Invalid card credentials! Please try again.");
 		}
 	}
 
-	public void login() {
-		Scanner scanner = new Scanner(System.in);
+	public void LoginU() {
 		System.out.println("Login to account: ");
+
 		System.out.println("Please enter your username: ");
 		String username = scanner.nextLine();
+
 		System.out.println("Please enter your passwords: ");
+
 		String password = scanner.nextLine();
-		user user = userDAO.usernames(username);
+		user user = userDAO.LoginU(username);
 		if (user != null && user.getPassword().equals(password)) {
 			System.out.println("Welcome " + username);
-		}
-		else {
+		} else {
 			System.out.println("Invalid credentials! Please try again!");
 		}
 	}
 
-
-   public static void main(String[] args) {
-		MusicLibraryApplication a = new MusicLibraryApplication();
-		Scanner scanner = new Scanner(System.in);
-
-		while (true) {
-			System.out.println("1. Register: ");
-			System.out.println("2. Login: ");
-			System.out.println("3: ");
-			System.out.println("4. : ");
-			System.out.println("5. : ");
-
-			int choice1 = scanner.nextInt();
-			scanner.nextLine();
-
-			switch (choice1) {
-				case 1:
-					a.register();
-					break;
-				case 2:
-					a.login();
-					break;
-			}
-		}
-   }
 }
